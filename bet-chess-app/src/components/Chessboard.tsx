d89333029b5f9ae93ad3508ee029877f0e067494
harrisb002 <harrisbe@sonoma.edu>
2024-02-20 16:31:17 -0800

    rendering notation

--- bet-chess-app/src/components/Chessboard.tsx
+++ bet-chess-app/src/components/Chessboard.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
-import './Chessboard.css'
+import "./Chessboard.css";
+
+const Xaxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const Yaxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 export default function Chessboard() {
-    return <div>
-        Here's the Chessboard.
-    </div>
+  let board = [];
+  
+  //loop for c
+  for (let j = Yaxis.length - 1; j >= 0; j--) {
+    for (let i = 0; i < Xaxis.length; i++) {
+      board.push(
+        <span>
+          {Xaxis[i]}
+          {Yaxis[j]}
+        </span>
+      );
+    }
+  }
+  return <div id="chessboard">{board}</div>;
 }
