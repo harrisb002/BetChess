7a4cc15c36bd411f1264175c844855a36ff11d1b
harrisb002 <harrisbe@sonoma.edu>
2024-02-20 20:11:51 -0800

    Rendering Pieces

--- bet-chess-app/src/components/Chessboard/Chessboard.tsx
+++ bet-chess-app/src/components/Chessboard/Chessboard.tsx
@@ -4,6 +4,19 @@ import "./Chessboard.css";
 const Xaxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const Yaxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
+// Used to store the starting position for the pieces
+interface Piece {
+  image: string;
+  XPosition: number;
+  YPosition: number;
+}
+
+// Store the starting positions for all the pieces
+const pieces: Piece[] = [];
+
+// Define the starting positions for all the pieces
+pieces.push({ image: "assets/images/pawn_b.png", XPosition: 0, YPosition: 1 });
+
 export default function Chessboard() {
   let board = [];
 
@@ -11,7 +24,17 @@ export default function Chessboard() {
   for (let j = Yaxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < Xaxis.length; i++) {
       const number = j + i + 2;
-      board.push(<Tile image="assets/images/pawn_b.png" number={number}/>)
+
+      let image = undefined;
+
+      // Loop through pieces array and place each piece in its position defined in array
+      pieces.forEach((element) => {
+        if (element.XPosition === i && element.YPosition === j) {
+          image = element.image;
+        }
+      });
+
+      board.push(<Tile image={image} number={number} />);
     }
   }
   return <div id="chessboard">{board}</div>;
