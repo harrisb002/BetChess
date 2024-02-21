f0e3d2a7d8f3202d7a30d04f8bf0b59afc2d3cd7
harrisb002 <harrisbe@sonoma.edu>
2024-02-20 19:36:07 -0800

    rendering checkerboard

--- bet-chess-app/src/components/Chessboard.tsx
+++ bet-chess-app/src/components/Chessboard.tsx
@@ -7,16 +7,27 @@ const Yaxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 export default function Chessboard() {
   let board = [];
-  
+
   //loop for c
   for (let j = Yaxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < Xaxis.length; i++) {
-      board.push(
-        <span>
-          {Xaxis[i]}
-          {Yaxis[j]}
-        </span>
-      );
+      const number = j + i + 2;
+
+      if (number % 2 === 0) {
+        board.push(
+          <div className="tile black-tile">
+            [{Xaxis[i]}
+            {Yaxis[j]}]
+          </div>
+        );
+      } else {
+        board.push(
+          <div className="tile white-tile">
+            [{Xaxis[i]}
+            {Yaxis[j]}]
+          </div>
+        );
+      }
     }
   }
   return <div id="chessboard">{board}</div>;
