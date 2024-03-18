68c52e41e1e8cd9e413c34f83680eecc16f4601e
harrisb002 <harrisbe@sonoma.edu>
2024-03-17 19:30:58 -0700

    Adding enPassant copy for Piece

--- bet-chess-app/src/models/Pawn.ts
+++ bet-chess-app/src/models/Pawn.ts
@@ -4,7 +4,11 @@ import { Position } from "./Position";
 
 export class Pawn extends Piece {
     enPassant?: boolean; // This is nullable
-    constructor(position: Position, side: Side) {
+    constructor(position: Position, side: Side, enPassant?: boolean) {
         super(position, PieceType.PAWN, side) // Initialize the base class 'Piece'
+    }
+
+    clone() : Pawn {
+        return new Pawn(this.position.clone(), this.side, this.enPassant)
     }
 }
